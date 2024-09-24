23d045f87d52328333b00ecedaddd12c2ba9c879
Vitaliy <100037026+BLiKf20@users.noreply.github.com>
2024-09-24 09:59:45 +0300

    Update NeoflexApplicationTests.java
    Add Tests

--- src/test/java/com/example/Neoflex/NeoflexApplicationTests.java
+++ src/test/java/com/example/Neoflex/NeoflexApplicationTests.java
@@ -2,6 +2,13 @@ package com.example.Neoflex;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.example.Neoflex.service.CalculateAmountOfVacationPayServiceImpl;
+import com.example.Neoflex.service.CalculateAmountOfVacationPayServiceInterface;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
 
 @SpringBootTest
 class NeoflexApplicationTests {
@@ -9,5 +16,52 @@ class NeoflexApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	CalculateAmountOfVacationPayServiceInterface calculate = new CalculateAmountOfVacationPayServiceImpl();
+
+	@Test
+	public void testServiceСalculateAmountOfVacationPayWithoutHolidays(){
+		String actual = calculate.calculateAmountOfVacationPay(
+				1200000.0, 21);
+		String expected = "Сумма отпускных без учета праздников составит: 71672,35";
+		Assertions.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testServiceСalculateAmountOfVacationPayWithHolidays(){
+
+		String actual = calculate.calculateAmountOfVacationPay(
+				1200000.0, 21,
+				LocalDate.of(2024, 06, 03));
+		String expected = "Сумма отпускных с учетом праздников составит: 68259,39";
+		Assertions.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testServiceСalculateAmountOfVacationInvalidNumberOfVacationsDay(){
+		String actual = calculate.calculateAmountOfVacationPay(
+				1200000.0, -21);
+		String expected = "Сумма отпускных без учета праздников составит: Введены не корректные данные";
+		Assertions.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testServiceСalculateAmountOfVacationInvalidNumberOfVacationsDayWithDate(){
+		String actual = calculate.calculateAmountOfVacationPay(
+				1200000.0, -21,
+				LocalDate.of(2024, 06, 03));
+		String expected = "Сумма отпускных с учетом праздников составит: Введены не корректные данные";
+		Assertions.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testServicecalculateAmountOfVacationInvalidAverageSalaryPerYear(){
+		String actual = calculate.calculateAmountOfVacationPay(
+				-1200000.0, 21,
+				LocalDate.of(2024, 06, 03));
+		String expected = "Сумма отпускных с учетом праздников составит: Введены не корректные данные";
+		Assertions.assertEquals(expected, actual);
+	}
+	
 
 }
